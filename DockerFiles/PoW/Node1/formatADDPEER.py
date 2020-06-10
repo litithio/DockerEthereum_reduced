1fe83da53617833ed6bf5a83a31f07e0578cde46
Johannes Ullrich <johannes.ullrich@diskart.de>
2020-06-10 12:26:37 +0200

    rebuild

---
+++ DockerFiles/PoW/Node1/formatADDPEER.py
@@ -0,0 +1,11 @@
+import os
+
+
+f = open("nodeinfo.txt", "r+")
+lines = f.readlines()
+text = lines[0]
+ip = "172.18.0.1"
+output = "admin.addPeer(" + text.split("@")[0] + "@" + ip + ":" + text.split("@")[1].split(":")[1] + ")"
+f.seek(0)
+f.write(output)
+f.truncate()
---
+++ scripts/deleteNode.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker rm -f Node1 Node2
